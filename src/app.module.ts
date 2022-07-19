5b0dc866d65d7307e451ff0ad62b4becf2a5a5fd
LeeJaeHoon <dlwogns3413@naver.com>
2022-07-19 17:20:12 +0900

    Feat - Project Setting clear

---
+++ src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import * as Joi from 'joi';
+import { User } from './users/entities/user.entity';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? '.env.dev'
+          : process.env.NODE_ENV === 'prod'
+          ? '.env.prod'
+          : '.env.test',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+      entities: [User],
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
+
+export class SkillName {
+  name?: string;
+}
+
+@Entity()
+export class User extends CoreEntity {
+  @IsEmail({}, { message: '이메일 형식이 아닙니다.' })
+  @IsNotEmpty({ message: '이메일은 비어있을 수 없습니다.' })
+  @Column({ unique: true })
+  email: string;
+
+  @IsString({ message: '이름은 문자열이어야 합니다.' })
+  @IsNotEmpty({ message: '이름은 비어있을 수 없습니다.' })
+  @Column()
+  nickname: string;
+}
